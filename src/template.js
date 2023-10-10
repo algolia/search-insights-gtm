dbe70546723fb556fd71178e02db5e681ca3fb13
Jason Berry <jason.berry@algolia.com>
2023-10-10 12:24:21 +1100

    fix: update logo (#17)
    * fix: update logo
    
    * update version to v1.4.1

--- src/template.js
+++ src/template.js
@@ -9,7 +9,7 @@ const getType = require('getType');
 const Math = require('Math');
 const Object = require('Object');
 
-const TEMPLATE_VERSION = '1.4.0';
+const TEMPLATE_VERSION = '1.4.1';
 const INSIGHTS_OBJECT_NAME = 'AlgoliaAnalyticsObject';
 const INSIGHTS_LIBRARY_URL =
   'https://cdn.jsdelivr.net/npm/search-insights@2.7.0';
