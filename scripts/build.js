114a5f96f7f50e9f87adb7619ce81edcbeb416e0
Jason Berry <jason.berry@algolia.com>
2024-11-25 15:34:18 +1100

    feat: convert to TS and enable versioning directly via package.json

--- scripts/build.js
+++ scripts/build.js
@@ -1,8 +1,22 @@
 const fs = require('fs-extra');
 const path = require('path');
-const { main: mainPath } = require('../package.json');
+const { devDependencies, main: mainPath, version } = require('../package.json');
+
+function escapeRegex(string) {
+  return string.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+}
+function getTemplateStringRegex(value) {
+  return new RegExp(
+    '`([^\\$`]*)(\\$\\{' + escapeRegex(value) + '\\})([^`]*)`',
+    'g'
+  );
+}
+function getTemplateStringReplacement(replacement) {
+  return `'$1${replacement}$3'`;
+}
 
 const ROOT_DIR = path.resolve('src');
+const BUILD_DIR = path.resolve('build');
 const DIST_PATH = path.resolve(mainPath);
 
 async function build() {
@@ -17,9 +31,23 @@ async function build() {
     path.join(ROOT_DIR, 'web-permissions.json')
   );
   const sandboxedJsContent = await fs.readFile(
-    path.join(ROOT_DIR, 'template.js')
+    path.join(BUILD_DIR, 'src', 'template.js')
   );
   const testContent = await fs.readFile(path.join(ROOT_DIR, 'tests.yaml'));
+
+  const replacedJsContent = String(sandboxedJsContent)
+    .replace(
+      "import { devDependencies, version } from '../package.json';\n",
+      ''
+    )
+    .replace(
+      getTemplateStringRegex("devDependencies['search-insights']"),
+      getTemplateStringReplacement(devDependencies['search-insights'])
+    )
+    .replace(
+      getTemplateStringRegex('version'),
+      getTemplateStringReplacement(version)
+    );
 
   const templateContent = [
     '___TERMS_OF_SERVICE___',
@@ -31,7 +59,7 @@ async function build() {
     '___WEB_PERMISSIONS___',
     webPermissionsContent,
     '___SANDBOXED_JS_FOR_WEB_TEMPLATE___',
-    sandboxedJsContent,
+    replacedJsContent,
     '___TESTS___',
     testContent,
   ].join('\n\n');
